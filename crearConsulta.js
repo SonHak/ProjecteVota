715f45bc80086a6a12cbafa9d0a70e81a418b684
Adrian Cardenas <SonHak@github.com>
2017-11-24 20:32:22 +0100

    consultas

--- crearConsulta.js
+++ crearConsulta.js
@@ -1,7 +1,6 @@
 //Variable global que cuenta cuantas respuestas (inputs) hay
 var cantidadRespuestas = 1;
 
-
 function crearConsulta(){
 	
 	//recogemos los labels, el textarea y los inputs y los guardamos en variables
@@ -9,90 +8,208 @@ function crearConsulta(){
 	var pregunta = document.getElementsByTagName("textarea");
 	var respuesta = document.getElementsByTagName("input");
 	
+	//hacemos que sean visibles los labels
 	for(var i=0;i<label.length;i++){
 		mostrar(label[i]);
 	}
 	
+	//hacemos que sean visibles las preguntas
 	for(var i=0;i<pregunta.length;i++){
 		mostrar(pregunta[i]);
-		pregunta[i].setAttribute("onBlur","mensajeError(event)");
-		pregunta[i].setAttribute("onFocus","quitarFondoRojo(event)");
 	}
-	
+
+	//hacemos que sean visibles las respuestas
 	for(var i=0;i<respuesta.length;i++){
 		mostrar(respuesta[i]);
 	}
 	
-	crearLabel();
-	crearInput();
+	//añadimos dos respuestas como mínimo obligatoriamente
+	añadirElemento(crearLabel(),crearInput());
+	añadirElemento(crearLabel(),crearInput());
 	
-	crearLabel();
-	crearInput();
+	//hacemos que el botón crear consulta quede deshabilitado
 	document.getElementById('crearConsulta').disabled = true;
+	
+	//creamos los siguientes botones
+	crearBotonRespuestas();
+	crearBotonEliminar();
+	crearBotonEnviar()
+	
+	//creamos los inputs y labels para las fechas
+	crearFechaInicio();
+	crearFechaFinal();
 }
 
+//la siguiente funcion crea el boton Crear Respuestas
+function crearBotonRespuestas(){
+	//creamos el elemento y lo guardamos en una variable
+	var boton = document.createElement("BUTTON");
+	//creamos texto para ese boton
+	var texto = document.createTextNode("Crear Respuestas");
+	//añadimos el texto anterior al boton
+	boton.appendChild(texto);
+	//le añadimos al boton el atributo onClick
+	boton.setAttribute("onclick","crearRespuesta()");
+	//añadimos el boton a su html para que sea visible
+	document.getElementById("botones").appendChild(boton);
+}
+
+//la siguiente funcion crea el boton Eliminar Respuestas
+function crearBotonEliminar(){
+	//creamos el elemento y lo guardamos en una variable
+	var boton = document.createElement("BUTTON");
+	//creamos texto para ese boton
+	var texto = document.createTextNode("Eliminar respuestas");
+	//añadimos el texto anterior al boton
+	boton.appendChild(texto);
+	//le añadimos al boton el atributo onClick
+	boton.setAttribute("onclick","eliminarRespuestas()");
+	//añadimos el boton a su html para que sea visible
+	document.getElementById("botones").appendChild(boton);
+}
+
+//la siguiente funcion crea el boton Enviar Datos
+function crearBotonEnviar(){
+	//creamos el elemento y lo guardamos en una variable
+	var boton = document.createElement("INPUT");
+	
+	//le añadimos al botón varios atributos
+	boton.setAttribute("type","submit");
+	boton.setAttribute("value","Enviar Datos");
+	boton.setAttribute("onclick","enviarDatos()");
+	
+	//recogemos el padre de ese elemento
+	var form = document.getElementsByTagName("FORM")[0];
+	//y lo insertamos al pricipio dentro del padre
+	form.insertBefore(boton,form.firstChild);
+}
+
+
+//la siguiente funcion cambia el estilo del elemento para que sea visible
 function mostrar(elemento){
 	elemento.style.display = "block";
 }
 
+//esta funcion crea nueva respuestas
 function crearRespuesta(){
-
-	crearLabel();
-	crearInput();
+	
+	//llamamos a la funcion añadir elemento pasandole como parametro las funciones crearLabel y crearInput
+	añadirElemento(crearLabel(),crearInput());
 
 }
+
+//la siguiente funcion crea un label nuevo
 function crearLabel(){
-	
-	var padre = document.getElementById("respuestas");
-	
+		
+	//creamos el elemento y lo guardamos en una variable
 	var labelNuevo = document.createElement("LABEL");
-	labelNuevo.setAttribute("id",cantidadRespuestas);
+
+	//creamos un texto nuevo y lo guardamos en una variable
 	var textoNuevo = document.createTextNode("Respuesta " + cantidadRespuestas + ":");
+	//añadimos dicho texto al nuevo label
 	labelNuevo.appendChild(textoNuevo);
 	
+	//sumamos 1 a la variable global cantidadRespuestas 
 	cantidadRespuestas++;
-	añadirElemento(labelNuevo,padre);
+	//devolvemos el nuevo label
+	return labelNuevo;
 	
 }
 
 function crearInput(){
-	var padre = document.getElementById("respuestas");
+	//creamos el elemento y lo guardamos en una variable
 	var inputNuevo = document.createElement("INPUT");
-	inputNuevo.setAttribute("id",cantidadRespuestas-1);
-	inputNuevo.setAttribute("onBlur","mensajeError(event)");
-	inputNuevo.setAttribute("onFocus","quitarFondoRojo(event)");
+	//le añadimos varios atributos al input
+	inputNuevo.setAttribute("class","respuesta");
+	inputNuevo.setAttribute("required","true");
+	//devolvemos el nuevo input
+	return inputNuevo;
 	
-	var saltoLinea = document.createElement("BR");
-	añadirElemento(inputNuevo,padre);
-	padre.appendChild(saltoLinea,padre);
 }
 
-function añadirElemento(elemento,padre){	
-	padre.appendChild(elemento,padre);
-}
+function añadirElemento(elemento1,elemento2){	
+	//guardamos en una variable el padre donde se van a crear el nuevo Label y el nuevo Input
+	var padre = document.createElement("DIV");
+	//le añadimos al padre un atributo id
+	padre.setAttribute("id",cantidadRespuestas-1);
+	
+	//añadimos los dos elementos (tanto el nuevo label como el nuevo input) al padre
+	padre.appendChild(elemento1);
+	padre.appendChild(elemento2);
 
-function quitarFondoRojo(event){
-	var elemento = event.currentTarget;
-	elemento.style.boxShadow = "none";
-}
-function mensajeError(event){
-	var elemento = event.currentTarget;
-	elemento.style.boxShadow = "0 0 5px red";
+	//por ultimo guardamos en una variable el padre general de cada div
+	var form = document.getElementsByTagName("FORM")[0];
+	//y se lo añadimos
+	form.appendChild(padre);
+
 }
 
 function eliminarRespuestas(){
-	var arrayInputs = document.getElementsByTagName("input");
-	var arrayLabels = document.getElementsByTagName("label");
 	
-	var prueba = arrayInputs.length;
+	//recorremos todos los divs que tenemos en el documento
+	var arrayDivs = document.getElementsByTagName("DIV");
 	
-	for(var i = prueba ; i > 2 ; i--){
-			eliminarElemento();
+	//por cada div que encuentre ejecutara una funcion (excepto para el DIV con ID "1" y el DIV con ID "2")
+	for(var i = arrayDivs.length; i > 3 ; i--){
+			eliminarElemento(arrayDivs[i-1]);
 	}
+	//devolvemos la variable global cantidadRespuestas a 3
+	cantidadRespuestas = 3;
+}
+
+function eliminarElemento(elemento){
+	//recoge el padre general del elemento pasado
+	var padre = document.getElementsByTagName("form")[0];
+	//y eliminamos el elemento
+	padre.removeChild(elemento);
 	
 }
-function eliminarElemento(){
-	var padre = document.getElementById("respuestas");
-	padre.removeChild(padre.lastChild);
-	padre.removeChild(padre.lastChild);
+
+function crearFechaInicio(){
+	var padre1 = document.getElementsByTagName("FORM")[0];
+	var padre2 = document.getElementsByTagName("input")[0];
+	
+	var label = document.createElement("LABEL");
+	label.appendChild(document.createTextNode("Fecha de Inicio: "));
+	
+	
+	var dia = crearInput();
+	var mes = crearInput();
+	var ano = crearInput();
+	
+	dia.setAttribute("placeholder","dia");
+	mes.setAttribute("placeholder","mes");
+	ano.setAttribute("placeholder","año");
+	
+	
+	
+	var saltoLinea = document.createElement("BR");
+	
+	padre1.insertBefore(label,padre2);
+	padre1.insertBefore(dia,padre2);
+	padre1.insertBefore(mes,padre2);
+	padre1.insertBefore(ano,padre2);
+	padre1.insertBefore(saltoLinea,padre2);
+}
+
+function crearFechaFinal(){
+	var padre1 = document.getElementsByTagName("FORM")[0];
+	var padre2 = document.getElementsByTagName("input")[1];
+	
+	var label = document.createElement("LABEL");
+	label.appendChild(document.createTextNode("Fecha de Cierre: "));
+	
+	var dia = crearInput();
+	var mes = crearInput();
+	var ano = crearInput();
+	
+	dia.setAttribute("placeholder","dia");
+	mes.setAttribute("placeholder","mes");
+	ano.setAttribute("placeholder","año");
+	
+	padre1.insertBefore(label,padre2);
+	padre1.insertBefore(input,padre2);
+	padre1.insertBefore(dia,padre2);
+	padre1.insertBefore(mes,padre2);
+	padre1.insertBefore(ano,padre2);
 }
--- crearConsulta.php
+++ crearConsulta.php
@@ -5,15 +5,14 @@
 	</style>
 	<body>
 		<?php 
-			echo "<button type='button' onclick='crearConsulta()' id='crearConsulta'>Crear Consulta </button>"; 
-			echo "<button type='button' onclick='crearRespuesta()'>Crear una Nueva Respuesta </button>"; 
-			echo "<button type='button' onclick='eliminarRespuestas()'>Eliminar Respuestas</button>";
+			echo "<div id='botones'>";
+				echo "<button type='button' onclick='crearConsulta()' id='crearConsulta'>Crear Consulta </button>"; 
+			echo "</div>";
 		?>
 		<?php
 			echo "<label for='consulta' class='consulta'>Crea la teva consulta:</label></br> \n";
 			echo "<textarea cols='40' rows='10' class='consulta'/> </textarea> \n";
-			echo "<div id='respuestas'>";
-			echo "</div>";
+			echo "<form method='GET' action='principal.php'></form>";
 		?>
 	</body>
 </html>
