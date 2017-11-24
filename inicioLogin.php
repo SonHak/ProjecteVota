506763e15b63a3e01d730ea9ae166dd8945ab74b
andrea-v-p <avidalpaniagua@iesesteveterradas.cat>
2017-11-24 17:17:23 +0100

    hola

--- inicioLogin.php
+++ inicioLogin.php
@@ -14,14 +14,29 @@
 		$pass = "Kemaku12";
 
 		$pdo = new PDO ("mysql:host=$hostname;dbname=$dbname", "$username", "$pass");
-		
 
-	 echo('<form action="principal.php" method="post" >
-			<p>Nickname: <input type="text" name="nick" required="true"></p>
+	if($_POST["email"] == null){	
+
+	 echo('<form action="inicioLogin.php" method="post" >
+			<p>EMAIL: <input type="text" name="email" required="true"></p>
 	    	<p>Password: <input type="password" name="password" required="true"></p>
 
 	        <input value="Login" type="submit" id="login" />
 	     </form>');
+
+	}else{
+		$query = $pdo->prepare("SELECT ID FROM Usuarios WHERE Email = '".$_POST["email"]."'");
+		$query->execute();
+		if($query){
+			$id = $query->fech();
+			$query2 = $pdo->prepare("SELECT ID FROM Usuarios WHERE ID = '".$id."' AND Password = '".$password = $_POST["password"]."'");
+			$query2->execute();
+			if($query2){
+				header('Location: principla.php');
+			}
+		}
+		
+	}
 
 
 	?>
