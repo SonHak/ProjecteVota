c3f6ddc8dae4b54df1dc81783484c0db844df185
andrea-v-p <avidalpaniagua@iesesteveterradas.cat>
2017-11-27 19:01:52 +0100

    hola

--- inicioLogin.php
+++ inicioLogin.php
@@ -12,6 +12,7 @@
 	
 		include("/var/www/html/ProjecteVota/config.php");
 
+		//FORMULARIO SI EL POST ESTA VACIO
 	if(!isset($_POST["email"])){	
 
 	 echo('<form action="inicioLogin.php" method="post" >
@@ -21,16 +22,20 @@
 	        <input value="Login" type="submit" id="login" />
 	     </form>');
 
+	 	//SI EL FORMULARIO ESTA RELLENO HACE UNA QUERY PARA RECOGER EL ID COMPROBANDO EL EMAIL
 	}else{
 		$query = $pdo->prepare("SELECT ID FROM Usuarios WHERE Email = '".$_POST["email"]."'");
 		$query->execute();
 		$id = $query->fetch();
 
+			//SI LA QUERY ANTERIOR NO FALLA CREA OTRA QUERY QUE RECOGE EL PASSWORD COMBROBANDO EL ID
 		if($id){
 			$query = $pdo->prepare("SELECT Password FROM Usuarios WHERE ID = ".$id[0]);
 			$query->execute();
 
 			$psswrd = $query->fetch();
+
+			//COMPRUEBA EL PASSWORD INTRODUCIDO CONTRA EL PASSWORD DE LA QUERY
 			if($psswrd[0] == $_POST["password"]){
 				try {
 					$query = $pdo->prepare("SELECT Email FROM Usuarios WHERE ID = ".$id[0]);
@@ -45,8 +50,8 @@
    					echo "Failed to get DB handle: " . $e->getMessage() . "\n";
     				exit;
   				}
-				// check
 			
+			//SI FALLA EL PASSWORD RECARGA EL FORMULARIO
 			}else{
 
 				 echo(' <p style="color:red; font-size:20px">Password erroneo!</p>
@@ -59,6 +64,8 @@
 			     </form>');
 
 			}
+
+			//SI FALLA EL EMAIL RECARGA EL FORMULARIO
 		} else {
 			echo(' <p style="color:red; font-size:20px">Email no encontrado!</p>
 				<br>
